3daa35dcf5b71813ccc3034f4f74b6c6648d6077
John Hayes <johnnyhayes7@gmail.com>
2019-02-14 15:14:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -44,6 +44,9 @@ class Triangle
   
   
   def kind 
+    if @side_one <= 0 && @side_two <= 0 && @side_three <= 0
+      raise TriangleError
+    elsif 
     if equilateral?
       return :equilateral
     elsif isosceles?
