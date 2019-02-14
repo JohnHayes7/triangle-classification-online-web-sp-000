912f3713101908e8b831e84844a44b0d21a36900
John Hayes <johnnyhayes7@gmail.com>
2019-02-14 15:12:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,75 @@
+require "pry"
+
 class Triangle
-  # write code here
+  
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  
+  def valid?
+    if (@side_one > 0 && @side_two > 0 && @side_three > 0) && ((@side_one + @side_two > @side_three) || (@side_two + @side_three > @side_one) || (@side_one + @side_three > @side_two))
+      return true 
+    else 
+      return false
+    end
+  end
+  
+  def equilateral?
+    if  valid? && (@side_one == @side_two && @side_two == @side_three)
+      return true
+    end
+  end
+  
+  def isosceles?
+    if valid? && ((@side_one == @side_two && @side_one != @side_three) || (@side_two == @side_three && @side_two != @side_one) || (@side_one == @side_three && @side_three != @side_two))
+      return true
+    end
+  end
+  
+  def inequality?
+    if (@side_one + @side_two < @side_three) || (@side_two + @side_three < @side_one) || (@side_one + @side_three < @side_two)
+      return true 
+    end
+  end
+  
+  def neg_or_no_size?
+     if @side_one <= 0 || @side_two <=0 || @side_three <= 0
+       return true 
+     end
+   end
+  
+  
+  def kind 
+    if equilateral?
+      return :equilateral
+    elsif isosceles?
+      return :isosceles
+    elsif valid?
+      return :scalene
+    else
+      raise TriangleError
+    end
+  end
+  
+  
+  
+  
+  class TriangleError<StandardError
+    
+    def triangle_inequality
+      "This triangle violates triangle inequality and is illegal"
+    end
+    
+    def negative_sides
+      "This triangle has a negative side and is illegal"
+    end
+    
+    def no_size
+      "One or more sides of this triangle has no size and is illegal"
+    end
+  end
 end
